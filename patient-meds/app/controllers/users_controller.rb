9fe639e75911e48eb02fa70c7a2b3ef77b6e69e6
ruizj3 <ruizj3@uw.edu>
2020-03-02 19:22:22 -0800

    Create user signup
    created user signup page

--- patient-meds/app/controllers/users_controller.rb
+++ patient-meds/app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
-      redirect to '/tweets'
+      redirect to '/meds'
     end
   end
 
